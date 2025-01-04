e6110fa4575c1da9c7bca3bc5b557bade69454e5
Tarun Teja Jangila <taruntejajangila@gmail.com>
2025-01-04 22:30:28 +0530

    tt'

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<div class="sidebar">
+    <h3>Expense Tracker</h3>
+    <ul>
+        <li><a href="index.php" class="active"><i class="fas fa-tachometer-alt"></i> Overview</a></li>
+        <li><a href="transactions.php"><i class="fas fa-exchange-alt"></i> Transactions</a></li>
+        <li><a href="accounts.php"><i class="fas fa-user"></i> Accounts</a></li>
+        <li><a href="credit_cards.php"><i class="fas fa-credit-card"></i> Credit Cards</a></li>
+        <li><a href="loans.php"><i class="fas fa-hand-holding-usd"></i> Loans</a></li>
+        <li><a href="shop_income.php"><i class="fas fa-store"></i> Shop Income</a></li>
+    </ul>
+</div>
